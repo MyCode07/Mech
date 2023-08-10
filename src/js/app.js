2c0fc9fc2105aceadc1d3c3180383232aff0ec27
MyCode07 <erem19992015@mail.ru>
2023-08-10 20:25:30 +0300

    about page

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,16 @@ import './utils/category-scroll.js';
 import './utils/fullscreen.js';
 import './utils/replace.js';
 import './utils/sliders.js';
+import './utils/ticker.js';
 
 // import './utils/animations.js';
 
 // import './utils/mouse.js';
-// import './utils/ticker.js';
 // import './utils/to-top.js';
 // import './utils/popup.js';
 // import './utils/inputmask.js';
 // import './utils/forms.js';
 // import './utils/accordeon.js';
 // import './utils/lazyImages.js';
+
+
