4086d94d4b637412f0f0c8d85b7ccd595890da49
MyCode07 <erem19992015@mail.ru>
2023-08-22 15:30:40 +0300

    change fullscreen sections layout

--- src/js/utils/category-scroll.js
+++ src/js/utils/category-scroll.js
@@ -1,25 +1,31 @@
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
+import { isMobile } from './isMobile.js';
 
 gsap.registerPlugin(ScrollTrigger);
 const processSlider = document.querySelector('.category__flex');
 
-if (window.innerWidth >= 768 && processSlider) {
+if (processSlider) {
     const slides = gsap.utils.toArray('.category__article');
+    if (slides.length) {
+        if (window.innerWidth >= 768 && !isMobile.any()) {
+            let x = slides[0].getBoundingClientRect().width * (slides.length - 1)
+            gsap.to(slides, {
+                x: -x,
+                ease: "none",
+                scrollTrigger: {
+                    trigger: processSlider,
+                    pin: true,
+                    start: "center center",
+                    scrub: 1,
+                    invalidateOnRefresh: true,
 
-    let x = slides[0].getBoundingClientRect().width * (slides.length - 1) - (processSlider.offsetWidth - slides[0].getBoundingClientRect().width)
-    gsap.to(slides, {
-        x: -x,
-        ease: "none",
-        scrollTrigger: {
-            trigger: processSlider,
-            pin: true,
-            start: "center center",
-            scrub: 1,
-            invalidateOnRefresh: true,
-
-            end: () => "+=" + processSlider.offsetWidth,
+                    end: () => "+=" + processSlider.offsetWidth,
+                }
+            });
         }
-    });
+        else {
+            processSlider.classList.add('_scroll')
+        }
+    }
 }
-
